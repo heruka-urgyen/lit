89ceb05ec2089c429bde4fa8743095a6a4fc5118
urgyen <imalyanov@gmail.com>
2020-08-15 21:45:49 -0500

    test: add test for commands/status/utils

---
+++ test/commands/status/utils.js
@@ -0,0 +1,49 @@
+import test from "ava"
+import sinon from "sinon"
+
+import p from "process"
+import * as g from "git-utils"
+import {runCommand, commit, commitFixup, updateLog} from "commands/status/utils"
+
+const gs = sinon.stub(g)
+const pauseSpy = sinon.spy(p.stdin, "pause")
+
+test("runCommand", t => {
+  gs.runCmd.returns({on: (_, f) => f()})
+  gs.gitStatus.returns({on: (_, f) => f("A 1.js\n")})
+  const update = sinon.spy()
+
+  runCommand("git add", "A 1.js\r", update)
+
+  t.truthy(gs.runCmd.calledWith({params: ["git add", "1.js"]}))
+  t.truthy(gs.gitStatus.called)
+  t.truthy(update.calledWith(["A 1.js"]))
+})
+
+test("commit", async t => {
+  gs.gitDiff.returns("+ 1")
+  const exit = sinon.spy()
+
+  commit(exit)
+  await t.truthy(gs.gitDiff.called)
+  t.truthy(pauseSpy.called)
+  await t.truthy(gs.gitCommit.called)
+  t.truthy(exit.called)
+})
+
+test("commit fixup", async t => {
+  const exit = sinon.spy()
+
+  commitFixup("123zxc", exit)
+  t.truthy(pauseSpy.called)
+  await t.truthy(gs.gitCommitFixup.calledWith("123zxc"))
+  t.truthy(exit.called)
+})
+
+test("updateLog", async t => {
+  gs.gitLog.returns("123zxc commit msg\n")
+  const update = sinon.spy()
+
+  await updateLog(update)
+  t.truthy(update.calledWith(["123zxc commit msg"]))
+})
