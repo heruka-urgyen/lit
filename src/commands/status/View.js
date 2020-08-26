39f1972619b094b96fc4f637789b1dcc7af78687
urgyen <imalyanov@gmail.com>
2020-08-26 11:22:18 -0500

    refactor: refactor handleInput in status

--- src/commands/status/View.js
+++ src/commands/status/View.js
@@ -3,114 +3,8 @@ import {Box, useApp, useInput} from "ink"
 import chalk from "chalk"
 
 import Selectable from "components/Selectable"
-import {gitHasStagedFiles} from "git-utils"
 
-import {runCommand, commit, commitAmend, commitFixup, updateLog} from "./utils"
-
-const selectDown = items => i => (i + 1) % items.length
-const selectUp = items => i => i > 0 ? i - 1 : items.length - 1
-
-const getInputConfig = props => async (input, key) => {
-  const {
-    exit,
-    mode,
-    selectItem,
-    setMode,
-    lines,
-    setLines,
-    selected,
-    log,
-    setLog,
-    allSelected,
-    toggleSelectAll,
-  } = props
-
-  if (input === "q") {
-    exit()
-  }
-
-  if (mode === "status") {
-    if (input === "j" || key.downArrow) {
-      selectItem(selectDown(lines))
-    }
-
-    if (input === "k" || key.upArrow) {
-      selectItem(selectUp(lines))
-    }
-
-    if (input === "a") {
-      toggleSelectAll()
-    }
-
-    if (input === "s") {
-      if (allSelected) {
-        runCommand("add", lines, setLines)
-      } else {
-        runCommand("add", [lines[selected]], setLines)
-      }
-    }
-
-    if (input === "r") {
-      if (allSelected) {
-        runCommand("reset", lines, setLines)
-      } else {
-        runCommand("reset", [lines[selected]], setLines)
-      }
-    }
-
-    if (input === "o") {
-      let res
-
-      if (allSelected) {
-        res = await runCommand("checkout", lines, setLines)
-      } else {
-        res = await runCommand("checkout", [lines[selected]], setLines)
-      }
-
-      const linesChanged = res.join() !== lines.join()
-
-      if (linesChanged) {
-        if (allSelected || res.length === 0) {
-          exit()
-        } else {
-          selectItem(selectDown(lines))
-        }
-      }
-    }
-
-    if (input === "c") {
-      commit(exit)
-    }
-
-    if (input === "m") {
-      commitAmend(exit)
-    }
-
-    if (input === "f") {
-      const hasStagedFiles = await gitHasStagedFiles()
-
-      if (hasStagedFiles) {
-        updateLog(setLog)
-        setMode("log")
-        selectItem(() => 0)
-      }
-    }
-  }
-
-  if (mode === "log") {
-    if (key.return) {
-      commitFixup(log[selected], exit)
-    }
-
-    if (input === "j" || key.downArrow) {
-      selectItem(selectDown(log))
-    }
-
-    if (input === "k" || key.upArrow) {
-      selectItem(selectUp(log))
-    }
-  }
-}
+import {runCommand, commit, commitAmend, commitFixup, updateLog, handleInput} from "./utils"
 
 export default function Status({state, actions, minHeight, maxHeight}) {
   const {exit} = useApp()
@@ -124,7 +18,7 @@ export default function Status({state, actions, minHeight, maxHeight}) {
     setLines,
   } = actions
 
-  useInput(getInputConfig({
+  useInput(handleInput({
     exit,
     mode,
     selectItem,
@@ -136,6 +30,11 @@ export default function Status({state, actions, minHeight, maxHeight}) {
     setLog,
     allSelected,
     toggleSelectAll,
+    runCommand,
+    commit,
+    commitAmend,
+    commitFixup,
+    updateLog,
   }))
 
   if (mode === "log") {
--- src/commands/status/utils.js
+++ src/commands/status/utils.js
@@ -53,3 +53,113 @@ export const updateLog = async update => {
   const output = await gitLog()
   update(output.split("\n").slice(0, -1))
 }
+
+const selectDown = items => i => (i + 1) % items.length
+const selectUp = items => i => i > 0 ? i - 1 : items.length - 1
+
+export const handleInput = props => async (input, key) => {
+  const {
+    exit,
+    mode,
+    selectItem,
+    setMode,
+    lines,
+    setLines,
+    selected,
+    log,
+    setLog,
+    allSelected,
+    toggleSelectAll,
+    runCommand,
+    commit,
+    commitAmend,
+    commitFixup,
+    updateLog,
+  } = props
+
+  if (input === "q") {
+    exit()
+  }
+
+  if (mode === "status") {
+    if (input === "j" || key.downArrow) {
+      selectItem(selectDown(lines))
+    }
+
+    if (input === "k" || key.upArrow) {
+      selectItem(selectUp(lines))
+    }
+
+    if (input === "a") {
+      toggleSelectAll()
+    }
+
+    if (input === "s") {
+      if (allSelected) {
+        runCommand("add", lines, setLines)
+      } else {
+        runCommand("add", [lines[selected]], setLines)
+      }
+    }
+
+    if (input === "r") {
+      if (allSelected) {
+        runCommand("reset", lines, setLines)
+      } else {
+        runCommand("reset", [lines[selected]], setLines)
+      }
+    }
+
+    if (input === "o") {
+      let res
+
+      if (allSelected) {
+        res = await runCommand("checkout", lines, setLines)
+      } else {
+        res = await runCommand("checkout", [lines[selected]], setLines)
+      }
+
+      const linesChanged = res.join() !== lines.join()
+
+      if (linesChanged) {
+        if (allSelected || res.length === 0) {
+          exit()
+        } else {
+          selectItem(s => Math.min(s, res.length - 1))
+        }
+      }
+    }
+
+    if (input === "c") {
+      commit(exit)
+    }
+
+    if (input === "m") {
+      commitAmend(exit)
+    }
+
+    if (input === "f") {
+      const hasStagedFiles = await gitHasStagedFiles()
+
+      if (hasStagedFiles) {
+        updateLog(setLog)
+        setMode("log")
+        selectItem(() => 0)
+      }
+    }
+  }
+
+  if (mode === "log") {
+    if (key.return) {
+      commitFixup(log[selected], exit)
+    }
+
+    if (input === "j" || key.downArrow) {
+      selectItem(selectDown(log))
+    }
+
+    if (input === "k" || key.upArrow) {
+      selectItem(selectUp(log))
+    }
+  }
+}
