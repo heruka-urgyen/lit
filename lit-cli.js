8bdc1cf5e24e0064341f4a4d1d01e3b50e89e672
urgyen <imalyanov@gmail.com>
2020-08-29 18:33:53 -0500

    build: add cli command for lit log

--- lit-cli.js
+++ lit-cli.js
@@ -18,6 +18,7 @@ const printHelp = () => {
   Commands
     status - interactive git status that allows to stage, reset, checkout, commit, amend, and fixup
     diff - interactive git diff that combines git status with diff for each file
+    log - interactive git log that allows inspecting diffs for each file in commit and simplifies interactive rebase
   `)
 }
 
@@ -32,6 +33,10 @@ const runCli = command => {
 
   if (command === "diff") {
     return require("./dist/diff")
+  }
+
+  if (command === "log") {
+    return require("./dist/log")
   }
 
   return printUsage()
